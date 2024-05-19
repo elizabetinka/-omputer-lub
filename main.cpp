90e6b6a3d1f82ef04454ca3a014962fe78eeb1a3
Елизавета Кравченкова <elizaveta.kravchenkova@mail.ru>
2024-05-19 21:31:40 +0300

    solve

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "ComputerClubInfo.h"
+
+int main(int argc, char** argv) {
+    if (argc < 2){
+        std::cerr<<"urequired parameters were not entered" << std::endl;
+        return 1;
+    }
+    ComputerClubInfo info;
+    try {
+        info.Parse(argv[1]);
+        info.Analysis();
+        info.Print();
+    }
+    catch (parce_error ex){
+        std::cerr<<"Произошла ошибка при чтении файла в строке: \n";
+        std::cout<<ex.what();
+    }
+    catch (std::runtime_error ex){
+        std::cerr<<ex.what();
+    }
+
+    return 0;
+}
